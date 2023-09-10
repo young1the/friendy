9882e0b057b0b5fce466d7e05cd9e87949b2bc2a
young1the <youngthedev@gmail.com>
2023-09-10 19:44:38 +0900

    init: project build

---
+++ src/main/java/com/chunjae/friendy/FriendyApplication.java
@@ -0,0 +1,13 @@
+package com.chunjae.friendy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chunjae/friendy/FriendyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chunjae.friendy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
