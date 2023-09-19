b971dec914e72294ec4092b02c381840551d919f
booheebaek <qorqngml@knou.ac.kr>
2023-09-19 13:12:07 +0900

    feat: 반경 3km내 학교 찾기 기능 추가

--- src/main/java/com/chunjae/friendy/util/coordinate/CoordinateController.java
+++ src/main/java/com/chunjae/friendy/util/coordinate/CoordinateController.java
@@ -3,31 +3,45 @@ package com.chunjae.friendy.util.coordinate;
 import com.chunjae.friendy.school.entity.SchoolAddress;
 import com.chunjae.friendy.school.service.SchoolService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RestController
 public class CoordinateController {
 
     @Autowired
     private SchoolService schoolService;
 
-    @GetMapping("/findSchoolsInRadius")
-    public String findSchoolsInRadius(Model model) {
-        double latitude = 37.4907391344;
-        double longitude = 127.0154235196;
-        double radius = 3.0; // 3KM 반경 설정
 
-        // 계산된 좌표 범위 내의 학교 주소 검색
+    @GetMapping("/aroundSchool")
+    public ResponseEntity<List<CoordinateDTO>> findAroundSchool(
+            @RequestParam double latitude,
+            @RequestParam double longitude) {
+
+        double radius = 3.0; // 3KM 반경 설정
         List<SchoolAddress> schoolsNearby = schoolService.findSchoolsInRadius(latitude, longitude, radius);
 
-        model.addAttribute("schools", schoolsNearby);
+        List<CoordinateDTO> coordinateDTOS = new ArrayList<>();
 
-        return "admin/pages/schools";
+        for (SchoolAddress schoolAddress : schoolsNearby) {
+            CoordinateDTO coordinateDTO = new CoordinateDTO(schoolAddress.getSchool_idx(), schoolAddress.getSchool().getName(), schoolAddress.getRoadAddress(), schoolAddress.getRoadZipCode(), schoolAddress.getLatitude(), schoolAddress.getLongitude());
+            coordinateDTOS.add(coordinateDTO);
+        }
+        return ResponseEntity.ok(coordinateDTOS);
     }
+
+
+
 }
+
+
+
+
+
 
 
