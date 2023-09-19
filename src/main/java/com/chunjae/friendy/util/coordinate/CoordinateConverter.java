0778ec99213936f137f2c65c7c2348c10d956ade
최세진 <osaejino123@gmail.com>
2023-09-19 09:33:44 +0900

    feat : 날씨 기능 완료
    날씨 기능 구현 완료

---
+++ src/main/java/com/chunjae/friendy/util/coordinate/CoordinateConverter.java
@@ -0,0 +1,55 @@
+package com.chunjae.friendy.util.coordinate;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ToString
+@Getter
+public class CoordinateConverter {
+    private static final double RE = 6371.00877; // 지구 반경(km)
+    private static final double GRID = 5.0;      // 격자 간격(km)
+    private static final double SLAT1 = 30.0;    // 투영 위도1(degree)
+    private static final double SLAT2 = 60.0;    // 투영 위도2(degree)
+    private static final double OLON = 126.0;    // 기준점 경도(degree)
+    private static final double OLAT = 38.0;     // 기준점 위도(degree)
+    private static final int XO = 43;            // 기준점 X좌표(GRID)
+    private static final int YO = 136;           // 기1준점 Y좌표(GRID)
+
+    public static Map<String, Double> convertToNxNy(double latitude, double longitude) {
+        double DEGRAD = Math.PI / 180.0;
+        double RADDEG = 180.0 / Math.PI;
+
+        double re = RE / GRID;
+        double slat1 = SLAT1 * DEGRAD;
+        double slat2 = SLAT2 * DEGRAD;
+        double olon = OLON * DEGRAD;
+        double olat = OLAT * DEGRAD;
+
+        double sn = Math.tan(Math.PI * 0.25 + slat2 * 0.5) / Math.tan(Math.PI * 0.25 + slat1 * 0.5);
+        sn = Math.log(Math.cos(slat1) / Math.cos(slat2)) / Math.log(sn);
+        double sf = Math.tan(Math.PI * 0.25 + slat1 * 0.5);
+        sf = Math.pow(sf, sn) * Math.cos(slat1) / sn;
+        double ro = Math.tan(Math.PI * 0.25 + olat * 0.5);
+        ro = re * sf / Math.pow(ro, sn);
+
+        double ra = Math.tan(Math.PI * 0.25 + (latitude) * DEGRAD * 0.5);
+        ra = re * sf / Math.pow(ra, sn);
+        double theta = longitude * DEGRAD - olon;
+        if (theta > Math.PI) theta -= 2.0 * Math.PI;
+        if (theta < -Math.PI) theta += 2.0 * Math.PI;
+        theta *= sn;
+
+        double x = Math.floor(ra * Math.sin(theta) + XO + 0.5);
+        double y = Math.floor(ro - ra * Math.cos(theta) + YO + 0.5);
+
+        Map<String, Double> result = new HashMap<>();
+        result.put("nx", x);
+        result.put("ny", y);
+
+        return result;
+    }
+
+}
